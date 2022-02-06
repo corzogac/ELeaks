c23ece530a227b01c6a7f5741633a909292b052e
corzogac <gerald.corzo@gmail.com>
2022-02-06 21:01:40 +0100

    Testing 1

--- Tools.py
+++ Tools.py
@@ -16,14 +16,15 @@ import time
 import wntr
 import pickle
 import numpy as np
+import pandas as pd
 
 
-def RunNet (x, OutPath):
+def RunNet (SimNet, OutPath):
     """RunNet is a function that simulates a network
     receive a FileNam and a Path to store the outputs 
 
     Args:
-        x ([Network Object from WNTR]): [A network file read
+        SimNet ([Network Object from WNTR]): [A network file read
         with wntr.network.WaterNetworkModel(<FileName>) ]
         OutPath ([Path String]): [Location to store the results]
 
@@ -32,17 +33,14 @@ def RunNet (x, OutPath):
         
                  Type= wntr.sim.results.SimulationResults object 
     """
-    sim = wntr.sim.EpanetSimulator(x)
-    if OutPath=='':        
-        pre=OutPath+'temp'
-    else:
-        pre=OutPath+'/temp'
+    sim = wntr.sim.EpanetSimulator(SimNet)
+    pre=OutPath+'temp'
 
     sim = sim.run_sim(version=2.2,file_prefix=pre ) 
     return sim
 
     
-def Baseline(Net, y = "No_defined"):
+def Baseline(Net, Dir = "OutputRuns/"):
     """This function generates a base line information
     1. Identifies and returns position of nodes with zero demand
     2. Returns the base demand 
@@ -60,56 +58,54 @@ def Baseline(Net, y = "No_defined"):
     x = Net
     BD = []
     Listnz=[]
-    if y == "No_defined":
-        for i in x.junction_name_list:        
+    for i in x.junction_name_list:        
             Node = x.get_node(i)
             if Node.base_demand>0:            
                 BD.append(Node.base_demand) 
                 Listnz.append(Node.name) 
             else:
-                BD.append(0)
-                
-    else:  
-        for i in y:
-            Node = x.get_node(i)
-            if Node.base_demand>0: 
-                BD.append(Node.base_demand) 
-            else: 
-                BD.append(0) 
+                BD.append(0)            
+    
     BD=np.array(BD)
     MBD=BD.mean()
-    
+
+    f=open(Dir+'wn.pkl','wb')
+    pickle.dump(Net,f)
+    f.close()
+
+    Net_Sim = RunNet(Net,Dir)  #Run network, base case.
+    f=open(Dir+'ns.pkl','wb')
+    pickle.dump(Net_Sim,f)
+    f.close()
+
     return BD, MBD, Listnz
 
 
 
-def EvaluateLeaks(RN=1,Lnz=''):
+def EvaluateLeaks(RN=1,Lnz='',Dir='OutputRuns/'):
     """[summary]
 
     Args:
-        RN (int, optional): [description]. Defaults to 1.
-        DirPath (str, optional): [description]. Defaults to 'test'.
+        RN (int, optional): [Run number to identify a sequence of runs in parallel]. Defaults to 1.
         Lnz (str, optional): [description]. Defaults to ''.
-    
+        Dir (str, path): "OutputRuns" 
     Result: Stores the result ina DF in the 
     """
     
-    nd=1
-    DirPath='OutputModel'
-
+    nd=1 #Number of days of the simulation
     start = time.time()  #start of the Timer, just for evaluate code performance. 
     #print(start)   
     
-    f=open(DirPath+'wn.pkl','rb')
+    f=open(Dir+'wn.pkl','rb')
     Net=pickle.load(f)
     f.close()
 
-    f=open(DirPath+'ns.pkl','rb')
+    f=open(Dir+'ns.pkl','rb')
     Net_Sim=pickle.load(f)
     f.close()
 
 
-    Net.options.hydraulic.demand_model=Method   #Using PDD as demand method
+    Net.options.hydraulic.demand_model='PDD'  #Using PDD as demand method
     Net.options.time.duration = nd*24*3600    #Time of simulation
     
     St = int(Net.options.time.duration/Net.options.time.hydraulic_timestep)    
@@ -127,8 +123,8 @@ def EvaluateLeaks(RN=1,Lnz=''):
     Sensor_Nodes = Lnz 
     print(len(Leak_Nodes))
     print(len(Sensor_Nodes))
-    Leakmin=Run/100
-    Leakmax=(Run+1)/100
+    Leakmin=RN/100
+    Leakmax=(RN+1)/100
     #dl=(Leakmax-Leakmin)/10
     LeakFlows = np.arange(Leakmin,Leakmax,0.001)
 
@@ -137,14 +133,15 @@ def EvaluateLeaks(RN=1,Lnz=''):
         for k in range(len(LeakFlows)):  
             #__________________      
             LeakFlow = [LeakFlows[k]/1000]*(24*nd+1) #array of the leak flow (m3/s)         
-            f=open(DirPath+'wn.pkl','rb')
+            f=open(Dir+'wn.pkl','rb')
             Net=pickle.load(f)
             f.close()
+            
             Net.add_pattern(name ='New', pattern = LeakFlow) #Add New Patter To the model
             Net.get_node(i).add_demand(base= 1, pattern_name='New') #Add leakflow
             Net.options.time.duration = 24*nd*3600 #Time of simulation
             #print(f'before run leak node no={i}, leak flow{k}')
-            Net_New = RunNet(Net,DirPath) # Run new model 
+            Net_New = RunNet(Net,Dir) # Run new model 
             #__________________________________________
             #   
             Net2 = Net_New.node['pressure'].loc[1:St*3600,Sensor_Nodes].\
